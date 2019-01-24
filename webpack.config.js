7d1fc4dd96c6f3db0fd215c9fb63469cd6288355
Ronald Suez <ingrsuezinfo@gmail.com>
2019-01-24 17:35:48 -0300

    initial-commit added library settings

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+var path = require('path');
+var webpack = require('webpack');
+module.exports = {
+  target: 'node',
+  entry: './lib/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'main.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        loader: 'babel-loader'
+      }
+    ]
+  },
+  stats: {
+    colors: true
+  },
+  devtool: 'source-map'
+};
